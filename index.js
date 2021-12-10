b12a652b230b9f106a1d628e2f71658e08b41559
Steven <78268115+yiywang@users.noreply.github.com>
2021-12-10 13:43:05 +0100

    Reconstruct the structure by Wang

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 const path = require('path')
 
-app.use('/',express.static(path.resolve(__dirname,'.../client')))
+app.use('/',express.static(path.resolve(__dirname,'./client')))
 
 const server = app.listen(9876)
 
