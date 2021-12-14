39f3959ff0cde6659b1370b123097a5a05e5b6a9
Test Nicolas <nicolas.gennart@hotmail.com>
2021-12-14 22:47:02 +0100

    not working

--- public/javascripts/chatroom.js
+++ public/javascripts/chatroom.js
@@ -6,8 +6,117 @@ const websocket = new WebSocket(serverUrl)
 const inputMessage = document.getElementById('message')
 const sendButton = document.getElementById('send')
 const dest = document.getElementById('dest')
+
 //contact.innerHTML = mainUser.userName
 
+
+var KeyHelper = window.libsignal.KeyHelper;
+
+function generateIdentity(store) {
+    return Promise.all([
+        KeyHelper.generateIdentityKeyPair(),
+        KeyHelper.generateRegistrationId(),
+    ]).then(function(result) {
+        store.put('identityKey', result[0]);
+        store.put('registrationId', result[1]);
+    });
+}
+
+function generatePreKeyBundle(store, preKeyId, signedPreKeyId) {
+    return Promise.all([
+        store.getIdentityKeyPair(),
+        store.getLocalRegistrationId()
+    ]).then(function(result) {
+        var identity = result[0];
+        var registrationId = result[1];
+
+        return Promise.all([
+            KeyHelper.generatePreKey(preKeyId),
+            KeyHelper.generateSignedPreKey(identity, signedPreKeyId),
+        ]).then(function(keys) {
+            var preKey = keys[0]
+            var signedPreKey = keys[1];
+
+            store.storePreKey(preKeyId, preKey.keyPair);
+            store.storeSignedPreKey(signedPreKeyId, signedPreKey.keyPair);
+
+            return {
+                identityKey: identity.pubKey,
+                registrationId : registrationId,
+                preKey:  {
+                    keyId     : preKeyId,
+                    publicKey : preKey.keyPair.pubKey
+                },
+                signedPreKey: {
+                    keyId     : signedPreKeyId,
+                    publicKey : signedPreKey.keyPair.pubKey,
+                    signature : signedPreKey.signature
+                }
+            };
+        });
+    });
+}
+
+    var ALICE_ADDRESS = new libsignal.SignalProtocolAddress("xxxxxxxxx", "1"); 
+    var BOB_ADDRESS   = new libsignal.SignalProtocolAddress("yyyyyyyyyyyyy", "1");
+
+    var aliceStore = new SignalProtocolStore();
+
+    var bobStore = new SignalProtocolStore();
+
+
+
+    var bobPreKeyId = 1337;
+    var bobSignedKeyId = 1;
+
+    var Curve = libsignal.Curve;
+
+        Promise.all([
+            generateIdentity(aliceStore),
+            generateIdentity(bobStore),
+        ]).then(function() {
+            return generatePreKeyBundle(bobStore, bobPreKeyId, bobSignedKeyId);
+        }).then(function(preKeyBundle) {
+            
+            var builder = new libsignal.SessionBuilder(aliceStore, BOB_ADDRESS);
+            return builder.processPreKey(preKeyBundle).then(function() {
+            
+                
+              var originalMessage = toArrayBuffer("my message ......");
+              var aliceSessionCipher = new libsignal.SessionCipher(aliceStore, BOB_ADDRESS);
+              var bobSessionCipher = new libsignal.SessionCipher(bobStore, ALICE_ADDRESS);
+
+              aliceSessionCipher.encrypt(originalMessage).then(function(ciphertext) {
+
+                  // check for ciphertext.type to be 3 which includes the PREKEY_BUNDLE
+                  return bobSessionCipher.decryptPreKeyWhisperMessage(ciphertext.body, 'binary');
+
+              }).then(function(plaintext) {
+
+                  alert(plaintext);
+
+              });
+
+              bobSessionCipher.encrypt(originalMessage).then(function(ciphertext) {
+
+                  return aliceSessionCipher.decryptWhisperMessage(ciphertext.body, 'binary');
+
+              }).then(function(plaintext) {
+
+                  assertEqualArrayBuffers(plaintext, originalMessage);
+
+              });
+
+            });
+        });
+function toArrayBuffer(buf) {
+    const ab = new ArrayBuffer(buf.length);
+    const view = new Uint8Array(ab);
+    for (let i = 0; i < buf.length; ++i) {
+        view[i] = buf[i];
+    }
+    return ab;
+}
 
 var mainUser = {
     userName: dest.getAttribute('data-value'),
@@ -156,3 +265,4 @@ function userButton(userId,userName){
     dest.innerHTML = userName
     sendTo_ = userId
 }
+
